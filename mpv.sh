95b7e1a13782b1fe1242f14de235108c5a0bfebc
skafiend <71149098+skafiend@users.noreply.github.com>
2025-03-28 16:15:46 +0300

    Update mpv.sh

--- mpv.sh
+++ mpv.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 _print_time() {
-    printf '%s%02d:%02d:%02d' "$1" "$2" "$3" "$4" 
+    printf '%s%2d:%02d:%02d' "$1" "$2" "$3" "$4" 
 }
-# mpv --title="mpv_audiobook" --input-ipc-server=/tmp/mpvsocket --save-position-on-quit -no-video "$(cat ~/.config/mpv/curbook.tmp)" &
-if pgrep -f "mpv_audiobook" > /dev/null; then
-    SOCK='/tmp/mpvsocket'
-    (
+
+SOCK='/tmp/mpvsocket'
+(
     metadata=$(echo '{ "command": ["get_property", "media-title"] }' | socat - $SOCK | jq -r .data) 
     position=$(echo '{ "command": ["get_property_string", "time-pos"] }' | socat - $SOCK  | jq -r .data | cut -d'.' -f 1) 
-#     jq extracts the number from 
-#     {"data":"8976.666122","request_id":0,"error":"success"}
-#     and cut using "." as a delimiter and "rounds up" the result
     remaining=$(echo '{ "command": ["get_property_string", "duration"] }' | socat - $SOCK | jq -r .data | cut -d'.' -f 1) 
     ERROR=$(</tmp/mpv_book_error)
     if [ -z $ERROR ]; then 
+        echo -n "%{F$PRIMARY_COLOR} " 
+        if [ ${#metadata} -gt 55 ]; then
+            printf '%.55s' "$metadata..."
+        else
+            printf "$metadata"
+        fi
         _print_time "%{F#fff}" $((position/3600)) $((position%3600/60)) $((position%60)) %{F-}
-        echo -n "%{F#f00} " 
-        printf '%s' "$metadata"
-        _print_time "%{F#fff} " $((remaining/3600)) $((remaining%3600/60)) $((remaining%60)) 
-        echo -n " %{F#f00}|%{F-}" 
+        echo -n " %{F$PRIMARY_COLOR}/%{F-}"
+        _print_time "" $((remaining/3600)) $((remaining%3600/60)) $((remaining%60))
+        echo -n " %{F$PRIMARY_COLOR}|"
     else 
         echo ""
     fi
 ) 2> /tmp/mpv_book_error
-else
-    echo ""
-fi
-
-
